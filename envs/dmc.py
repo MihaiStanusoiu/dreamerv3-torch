e229c1fa9ec5a44d7e9e756b806d282fd7ee1438
Mihai-Teodor Stanusoiu <stanusoiumihai@gmail.com>
2024-10-09 11:44:06 +0200

    Headless mode

--- envs/dmc.py
+++ envs/dmc.py
@@ -54,7 +54,7 @@ class DeepMindControl:
                 break
         obs = dict(time_step.observation)
         obs = {key: [val] if len(val.shape) == 0 else val for key, val in obs.items()}
-        obs["image"] = self.render()
+        # obs["image"] = self.render()
         # There is no terminal state in DMC
         obs["is_terminal"] = False if time_step.first() else time_step.discount == 0
         obs["is_first"] = time_step.first()
@@ -66,7 +66,7 @@ class DeepMindControl:
         time_step = self._env.reset()
         obs = dict(time_step.observation)
         obs = {key: [val] if len(val.shape) == 0 else val for key, val in obs.items()}
-        obs["image"] = self.render()
+        # obs["image"] = self.render()
         obs["is_terminal"] = False if time_step.first() else time_step.discount == 0
         obs["is_first"] = time_step.first()
         return obs
